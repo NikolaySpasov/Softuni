ac39938dffc97208bb6dbc307dcb5ed1e28dd13d
NikolaySpasov <nikspa@gmail.com>
2017-02-02 23:24:07 +0200

    second

--- Programming Fundamentals/02.ProgramFundamDataTypesAndVariables-Exer/17. Cypher Roulette/CypherRoulette.cs
+++ Programming Fundamentals/02.ProgramFundamDataTypesAndVariables-Exer/17. Cypher Roulette/CypherRoulette.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _17.Cypher_Roulette
+class CypherRoulette
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var n = int.Parse(Console.ReadLine());
+
+        var result = string.Empty;
+        var line = string.Empty;
+
+        var isAppendingToTheEnd = true;
+
+        while (n > 0)
         {
+            var previousLine = line;
+
+            line = Console.ReadLine();
+
+            if (line == previousLine)
+            {
+                result = string.Empty;
+
+                if (line != "spin")
+                {
+                    n--;
+                }
+
+                continue;
+            }
+
+            if (line == "spin")
+            {
+                isAppendingToTheEnd = !isAppendingToTheEnd;
+                continue;
+            }
+
+            result = isAppendingToTheEnd ? result + line : line + result;
+            n--;
         }
+
+        Console.WriteLine(result);
     }
 }
