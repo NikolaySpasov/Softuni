a67786c2a445e5dbc26ac84407f85b191926d64a
NikolaySpasov <nikspa@gmail.com>
2017-06-07 21:17:59 +0300

    Update Manual string Processing

--- C# Advanced/03. MATRICES/03. GroupNumbers/Program.cs
+++ C# Advanced/03. MATRICES/03. GroupNumbers/Program.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _03.GroupNumbers
+﻿namespace lab03_GroupNumbers
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    public class GroupNumbers
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            // This solution is prity sly !!!
+
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int[] input = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string residue0 = "";
+            string residue1 = "";
+            string residue2 = "";
+
+            foreach (var num in input)
+            {
+                int rest = num % 3;
+                if (rest == 0)
+                {
+                    residue0 = residue0 + num.ToString() + " ";
+                }
+                else if (rest == 1 || rest == -1)
+                {
+                    residue1 = residue1 + num.ToString() + " ";
+                }
+                else
+                {
+                    residue2 = residue2 + num.ToString() + " ";
+                }
+            }
+
+            Console.WriteLine(residue0.TrimEnd());
+            Console.WriteLine(residue1.TrimEnd());
+            Console.WriteLine(residue2.TrimEnd());
+
+            //watch.Stop();
+            //Console.WriteLine(watch.ElapsedTicks);
         }
     }
 }
