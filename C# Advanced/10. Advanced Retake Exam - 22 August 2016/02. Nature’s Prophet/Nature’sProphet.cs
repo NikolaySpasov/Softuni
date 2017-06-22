5a9f90f100b7dc75326848cc8c7cae4d98a912f3
NikolaySpasov <nikspa@gmail.com>
2017-06-22 07:08:24 +0300

    upload C# advance retake Exam 22 August 2016

---
+++ C# Advanced/10. Advanced Retake Exam - 22 August 2016/02. Nature’s Prophet/Nature’sProphet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public sealed class Startup
+{
+    private static void Main(string[] args)
+    {
+        int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[,] garden = new int[dimensions[0], dimensions[1]];
+        string input = Console.ReadLine();
+
+        while (input != "Bloom Bloom Plow")
+        {
+            int[] tokens = input.Split(' ').Select(int.Parse).ToArray();
+            int row = tokens[0];
+            int col = tokens[1];
+
+            for (int r = 0; r < garden.GetLength(0); r++)
+            {
+                garden[r, col]++;
+            }
+
+            for (int c = 0; c < garden.GetLength(1); c++)
+            {
+                garden[row, c]++;
+            }
+
+            garden[row, col]--;
+
+            input = Console.ReadLine();
+        }
+        var result = new StringBuilder();
+        for (int row = 0; row < garden.GetLength(0); row++)
+        {
+            for (int col = 0; col < garden.GetLength(1); col++)
+            {
+                result.Append(garden[row, col] + " ");
+            }
+
+            result.AppendLine();
+        }
+
+        Console.WriteLine(string.Join(" ",result));
+    }
+}
---
+++ C# Advanced/10. Advanced Retake Exam - 22 August 2016/04. Ashes of Roses/AshesOfRoses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+internal class AshesOfRoses
+{
+    private static void Main()
+    {
+        var inputLine = Console.ReadLine();
+        Dictionary<string, Dictionary<string, long>> flowers = new Dictionary<string, Dictionary<string, long>>();
+
+        while (inputLine != "Icarus, Ignite!")
+        {
+            Regex regex = new Regex(@"\b^Grow <([A-Z][a-z]*)> <([a-zA-Z\d]+)> (\d+)$");
+
+            Match match = regex.Match(inputLine);
+
+            while (match.Groups[0].Value == "")
+            {
+                inputLine = Console.ReadLine();
+                if (inputLine == "Icarus, Ignite!")
+                {
+                    break;
+                }
+                match = regex.Match(inputLine);
+            }
+            if (inputLine == "Icarus, Ignite!")
+            {
+                break;
+            }
+            string region = match.Groups[1].Value;
+            string color = match.Groups[2].Value;
+            long number = long.Parse(match.Groups[3].Value);
+
+            if (!flowers.ContainsKey(region))
+            {
+                flowers.Add(region, new Dictionary<string, long>());
+            }
+            if (!flowers[region].ContainsKey(color))
+            {
+                flowers[region].Add(color, 0);
+            }
+            flowers[region][color] += number;
+
+            inputLine = Console.ReadLine();
+        }
+
+        foreach (var outerPair in flowers.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(y => y.Key))
+        {
+            Console.WriteLine("{0}", outerPair.Key);
+
+            foreach (var innerPair in outerPair.Value.OrderBy(x => x.Value).ThenBy(y => y.Key))
+            {
+                Console.WriteLine("*--{0} | {1}", innerPair.Key, innerPair.Value);
+            }
+        }
+    }
+}
