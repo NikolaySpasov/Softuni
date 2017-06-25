fd0b3cfb032031a8e3823f5b54d90519cec13b72
NikolaySpasov <nikspa@gmail.com>
2017-06-25 23:34:27 +0300

    Exam

---
+++ C# Advanced/Exam/Exam/01. Regeh.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var inputLine = Console.ReadLine();
+        var indexes = new List<int>();
+        var stack = new Stack<int>();
+        var listForMatch = string.Empty;
+        for (int i = 0; i < inputLine.Length; i++)
+        {
+            if (inputLine[i] == '[')
+            {
+                stack.Push(i);
+            }
+            else if (inputLine[i] == ']')
+            {
+                int startIndex = stack.Pop();
+                string stringToMatch = inputLine.Substring(startIndex, i - startIndex + 1);
+                listForMatch += listForMatch;
+            }
+        }
+        MatchCollection matches = Regex.Matches(listForMatch, @"\[\S+<(\d+)REGEH(\d+)>\S+]");
+
+
+        foreach (Match match in matches)
+        {
+            indexes.Add(int.Parse(match.Groups[1].Value));
+            indexes.Add(int.Parse(match.Groups[2].Value));
+        }
+
+        var sb = new StringBuilder();
+        var count = 1;
+
+        for (int i = 0; i < indexes.Count; i++)
+        {
+            if (indexes[i] <= inputLine.Length)
+            {
+                if (indexes[i] != indexes[indexes.Count - 1])
+                {
+                    sb.Append((char)inputLine[indexes[i]]);
+                    indexes[i + 1] += indexes[i];
+                }
+                else if (indexes[i] == indexes[indexes.Count - 1])
+                {
+                    if (indexes[indexes.Count - 1] == inputLine.Length)
+                    {
+                        sb.Append((char)inputLine[2]);
+                        break;
+                    }
+                    sb.Append((char)inputLine[indexes[i]]);
+                }
+
+
+            }
+            if (indexes[i] > inputLine.Length)
+            {
+                sb.Append((char)inputLine[indexes[i] % (inputLine.Length) + count]);
+                count++;
+            }
+
+        }
+        Console.WriteLine(sb);
+    }
+}
---
+++ C# Advanced/Exam/Task 4/Hospital.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        var inputLine = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var hospital = new Dictionary<string, Dictionary<string, List<string>>>();
+
+
+        while (inputLine[0] != "Output")
+        {
+            //{Department} {Doctor} {Patient}
+            string department = inputLine[0];
+            string doctor = inputLine[1] + inputLine[2];
+            string patient = inputLine[3];
+
+            if (!hospital.ContainsKey(department))
+            {
+                hospital.Add(department, new Dictionary<string, List<string>>());
+            }
+            if (!hospital[department].ContainsKey(doctor))
+            {
+                hospital[department][doctor] = new List<string>();
+            }
+            hospital[department][doctor].Add(patient);
+
+            inputLine = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        var outputCommand = Console.ReadLine();
+        while (outputCommand != "End")
+        {
+            foreach (var kvp in hospital )
+            {
+                var department = kvp.Key;
+                var doctor = kvp.Value;
+
+
+                foreach (var innerKvp in doctor)
+                {
+                    if (outputCommand == department)
+                    {
+                        var patient = hospital[department].Select(p => p.Value);
+                        Console.WriteLine(string.Join(Environment.NewLine, patient));
+
+                    }
+                    //if (outputCommand == innerKvp.Key)
+                    //{
+                    //    var patient = hospital[department].Select(p => p.Value).ToList<string>();
+                    //    Console.WriteLine(string.Join(Environment.NewLine, patient));
+                    //}
+                }
+                    
+                
+
+            }
+
+            outputCommand = Console.ReadLine();
+        }
+
+    }
+
+}
+public static class Hospital
+{
+
+}
