5b153651a61a7a9cf1514ab9af077be86aca3310
NikolaySpasov <nikspa@gmail.com>
2017-07-12 18:56:41 +0300

    Update

---
+++ C# OOP Basics/07. Exercise - Inheritance/04. Online Radio Database/OnlineRadioDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace p04_OnlineRadioDatabase
+{
+    using System;
+
+    public class OnlineRadioDatabase
+    {
+        public static void Main()
+        {
+            List<Song> playList = new List<Song>();
+
+            int numberOfSongs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] input = Console.ReadLine().Split(';').Select(x => x.Trim()).ToArray();
+
+                try
+                {
+                    Song newSong = new Song(input[0], input[1], input[2]);
+                    playList.Add(newSong);
+                    Console.WriteLine("Song added.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine($"Songs added: {playList.Count}");
+
+            int totalLength = playList.Count > 0 ? playList.Sum(x => x.TotalSongLengthInSeconds) : 0;
+            int hours = totalLength / 3600;
+            int min = (totalLength / 60) % 60;
+            int sec = totalLength % 60;
+            Console.WriteLine($"Playlist length: {hours}h {min}m {sec}s");
+
+
+        }
+    }
+}
---
+++ C# OOP Basics/07. Exercise - Inheritance/04. Online Radio Database/Song.cs
@@ -0,0 +1,50 @@
+﻿namespace p04_OnlineRadioDatabase
+{
+    using System;
+
+    public class Song
+    {
+        private string artistName;
+        private string songName;
+        private int totalSongLengthInSeconds;
+
+        public Song(string artistName, string songName, string time)
+        {
+            this.ArtistName = artistName;
+            this.SongName = songName;
+            this.TotalSongLengthInSeconds = Validator.TimeValidate(time);
+        }
+
+        public string ArtistName
+        {
+            get { return this.artistName; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 20)
+                {
+                    throw new ArgumentException("Artist name should be between 3 and 20 symbols.");
+                }
+                this.artistName = value;
+            }
+        }
+
+        public string SongName
+        {
+            get { return this.songName; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 30)
+                {
+                    throw new ArgumentException("Song name should be between 3 and 30 symbols.");
+                }
+                this.songName = value;
+            }
+        }
+
+        public int TotalSongLengthInSeconds
+        {
+            get { return this.totalSongLengthInSeconds; }
+            private set { this.totalSongLengthInSeconds = value; }
+        }
+    }
+}
